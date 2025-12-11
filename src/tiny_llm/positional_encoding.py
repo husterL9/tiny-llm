30c48452396e340c678fe7960eecdae4790251c6
nivis <nivis@192.168.0.101>
2025-12-12 00:47:31 +0800

    feat: week1 day2 task1 done

--- src/tiny_llm/positional_encoding.py
+++ src/tiny_llm/positional_encoding.py
@@ -9,9 +9,34 @@ class RoPE:
         base: int = 10000,
         traditional: bool = False,
     ):
-        pass
-
+        assert dims % 2 == 0
+        half_dim=dims//2
+        base_freqs = 1.0 / (base ** (mx.arange(0, half_dim) / half_dim))
+        # positions: (L, 1)
+        positions=mx.arange(0,seq_len)[:,None]
+        freqs=positions*base_freqs[None,:]
+        self.cos_freqs,self.sin_freqs=mx.cos(freqs), mx.sin(freqs)
+        
     def __call__(
         self, x: mx.array, offset: list[slice] | slice | None = None
     ) -> mx.array:
-        pass
+        x = x.transpose(0, 2, 1, 3)
+        N, H, L, D = x.shape
+        half_dim = D // 2
+        if offset is None:
+            cos = self.cos_freqs[:L]      # (L, D/2)
+            sin = self.sin_freqs[:L]
+        else:
+            cos = self.cos_freqs[offset]  # (L_slice, D/2)
+            sin = self.sin_freqs[offset]
+        
+        cos = cos[None, None, :, :]       # (1,1,L,D/2)
+        sin = sin[None, None, :, :]       # (1,1,L,D/2)
+
+        x_even = x[..., 0::2]             # (N,H,L,D/2)
+        x_odd  = x[..., 1::2]             # (N,H,L,D/2)
+        x_even_rotary = x_even * cos - x_odd * sin
+        x_odd_rotary = x_even * sin + x_odd * cos
+        x_rotary=mx.stack([x_even_rotary, x_odd_rotary], axis=-1)
+        x_rotary = x_rotary.reshape(N, H, L, D)                   # (N,H,L,D)
+        return x_rotary.transpose(0,2,1,3)

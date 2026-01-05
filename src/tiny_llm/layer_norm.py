42d9eedf28968ac456df5d6a84deb94074bc1a38
nivis <nivis@192.168.0.101>
2026-01-05 20:22:44 +0800

    feat: week1 day4 task1

--- src/tiny_llm/layer_norm.py
+++ src/tiny_llm/layer_norm.py
@@ -3,7 +3,19 @@ import mlx.core as mx
 
 class RMSNorm:
     def __init__(self, dim: int, weight: mx.array, eps: float = 1e-5):
-        pass
+        self.dim=dim
+        #(D,)
+        self.weight=weight
+        self.eps=eps
 
     def __call__(self, x: mx.array) -> mx.array:
-        pass
+        x_f32 = x.astype(mx.float32)
+        #(N..,)
+        mean_sq = mx.mean(x_f32 * x_f32, axis=-1,keepdims=True)
+        rms = mx.sqrt(mean_sq + self.eps) 
+        #(N..,D)
+        y = (x_f32 / rms).astype(x.dtype)
+        y = y * self.weight
+        return y
+
+        

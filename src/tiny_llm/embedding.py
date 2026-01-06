483cb504a257e49c1dcbb9e02fcaffeac95b3ba2
nivis <nivis@192.168.0.101>
2026-01-06 18:46:13 +0800

    feat: week1 day5 task2 done

--- src/tiny_llm/embedding.py
+++ src/tiny_llm/embedding.py
@@ -1,12 +1,23 @@
 import mlx.core as mx
 
+# Embedding::__call__
+# weight: vocab_size x embedding_dim
+# Input: N.. (tokens)
+# Output: N.. x embedding_dim (vectors)
+
+# Embedding::as_linear
+# weight: vocab_size x embedding_dim
+# Input: N.. x embedding_dim
+# Output: N.. x vocab_size
 
 class Embedding:
     def __init__(self, vocab_size: int, embedding_dim: int, weight: mx.array):
-        pass
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.weight = weight
 
     def __call__(self, x: mx.array) -> mx.array:
-        pass
+        return mx.take(self.weight, x, axis=0)
 
     def as_linear(self, x: mx.array) -> mx.array:
-        pass
+        return mx.matmul(x,self.weight.T)

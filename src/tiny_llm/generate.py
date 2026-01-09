384c4c4e7f8c6976405c9b8dea02012140442e28
nivis <nivis@192.168.0.102>
2026-01-09 22:54:28 +0800

    feat:week1 day6 done

--- src/tiny_llm/generate.py
+++ src/tiny_llm/generate.py
@@ -12,7 +12,23 @@ def simple_generate(
     sampler: Callable[[mx.array], mx.array] | None,
 ) -> str:
     def _step(model, y):
-        pass
+        output_logits=model(y)
+        logits = output_logits[:, -1, :]
+        # (1,1)
+        next_token = mx.argmax(logits, axis=-1,keepdims=True)
+        return next_token 
+    detokenizer = tokenizer.detokenizer
+    detokenizer.reset()
+    inputs=mx.array(tokenizer.encode(prompt))
+    inputs = inputs[None, :]
+    while(True):
+       next_token = _step(model,inputs)
+       inputs = mx.concatenate([inputs, next_token],axis=1)
+       if next_token.item()==tokenizer.eos_token_id:
+           break
+       detokenizer.add_token(next_token.item())
+       print(detokenizer.last_segment, end="", flush=True)
+
 
 
 def simple_generate_with_kv_cache(
